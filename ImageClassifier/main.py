9383a00cad89cf41846c12eb99a44705d3c37799
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-19 11:12:08 -0300

    Set up flask folders

--- ImageClassifier/main.py
+++ ImageClassifier/main.py
@@ -1,4 +1,13 @@
 from classifier import *
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
-    c = Classifier();
+    # c = Classifier();
+    app.run()
