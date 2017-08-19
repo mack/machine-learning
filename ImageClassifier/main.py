2d1198af87844b7bf4f97a782023468c0a85af8e
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-19 11:56:22 -0300

    Prettified the html/css

--- ImageClassifier/main.py
+++ ImageClassifier/main.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", pageType='test2')
 
 
 if __name__ == '__main__':
     # c = Classifier();
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run()
