07188fef7c7edc485f66e161df00cc74ab916716
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-19 12:51:59 -0300

    Switch from client side js to server side python image uploads

--- ImageClassifier/main.py
+++ ImageClassifier/main.py
@@ -1,12 +1,27 @@
 from classifier import *
-from flask import Flask, render_template
+from PIL import Image
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-@app.route('/')
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html", pageType='test2')
+    if (request.method == "POST"):
+        if 'photo' not in request.files:
+            return redirect(request.url)
+        photo = request.files['photo']
+        if photo.filename == '':
+            return redirect(request.url)
+        if photo and allowed_file(photo.filename):
+            print('Yay!')
+        else:
+            print('Awh...')
 
+    return render_template("index.html", pageType='test2')
 
 if __name__ == '__main__':
     # c = Classifier();
--- ImageClassifier/static/js/main.js
+++ ImageClassifier/static/js/main.js
@@ -1,25 +1,15 @@
 function loadImage(){
-    var file = document.querySelector('input[type=file]').files[0];
-    var reader = new FileReader();
+    var file = document.querySelector('input[type=file]').files[0]
+    var reader = new FileReader()
 
     if (file) {
-        reader.readAsDataURL(file); //reads the data as a URL
+      console.log(file)
+      reader.readAsDataURL(file)
     }
 
     reader.onloadend = function () {
         src = reader.result;
-        $('#preview').append("<img src=" + src + ">")
-        $('img').css({
-          "vertical-align" : "top",
-          "display" : "block",
-          "max-width" : "300%",
-          "max-height" : "100%",
-          "position" : "absolute",
-          "top" : "0",
-          "bottom" : "0",
-          "left" : "0",
-          "right" : "0"
-        })
         $('.submit').text("Try another?")
+        $('form').submit()
     }
 }
