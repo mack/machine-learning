788ecf64e8b1ab744169de73b2ca28bb284b91c2
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-23 20:49:38 -0300

    Trained the model and now can run strings for sentiment analysis on it

--- ImprovedSentimentAnalysis/main.py
+++ ImprovedSentimentAnalysis/main.py
@@ -7,22 +7,12 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    # load in data
-    train_data = pd.read_csv("labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
-    test_data = pd.read_csv("testData.tsv", header=0, delimiter="\t", quoting=3)
+    forest = joblib.load('sentiment.pkl')
+    vectorizer = joblib.load('sentiment_vectorizer.pkl')
 
-    # format data
-    train = []
-    for i in range(0, len(train_data["review"])):
-        train.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train_data['review'][i], True)))
-
-    vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None, max_features = 5000)
-    train_features = vectorizer.fit_transform(train)
-    np.asarray(train_features)
-
-    # train model
-    forest = RandomForestClassifier(n_estimators=100)
-    forest = forest.fit(train_features, train_data["sentiment"])
-
-    # save the model with sklearn joblib
-    joblib.dump(forest, 'sentiment.pkl')
+    prediction_str = "This was a terrible movie. I would not watch it."
+    prediction_str = [" ".join(KaggleWord2VecUtility.review_to_wordlist(prediction_str, True))]
+    prediction_str = vectorizer.transform(prediction_str)
+    np.asarray(prediction_str)
+    result = forest.predict(prediction_str)
+    print(result)
