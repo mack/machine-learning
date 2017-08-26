a1bb0732bbd8a3590733ae7f231634aab976d09b
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-25 21:19:38 -0300

    Added training code

--- MNISTBeginner/main.py
+++ MNISTBeginner/main.py
@@ -1,4 +1,4 @@
-# logistic regression on mnist instead of a superior deep convolution net  
+# logistic regression on mnist instead of a superior deep convolution net
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
@@ -10,6 +10,17 @@ def run():
     W = tf.Variable(tf.zeros([784, 10]))
     b = tf.Variable(tf.zeros([10]))
 
+    y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+    sess = tf.InteractiveSession()
+    for _ in range(1000):
+      batch_xs, batch_ys = mnist.train.next_batch(100)
+      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+      
 def load_input():
     # Load and save mnist
     mnist = input_data.read_data_sets("data/", one_hot=True)
