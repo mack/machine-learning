b04fa8caec286fc9ace980e482816e22018813ac
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-26 22:55:19 -0300

    Trying to get tensorflow working

--- MNISTBeginner/main.py
+++ MNISTBeginner/main.py
@@ -10,25 +10,25 @@ def index():
     return render_template('index.html')
 
 
-def run():
+def main():
     mnist = input_data.read_data_sets("data/", one_hot=True)
     # create variables + constants
     x = tf.placeholder(tf.float32, [None, 784])
-    # start off with weights and biases at 0
     W = tf.Variable(tf.zeros([784, 10]))
     b = tf.Variable(tf.zeros([10]))
-
-    y = tf.nn.softmax(tf.matmul(x,W) + b)
+    y = tf.matmul(x, W) + b
 
     y_ = tf.placeholder(tf.float32, [None, 10])
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
     sess = tf.InteractiveSession()
-    for _ in range(1000):
+
+    for _ in range(1):
       batch_xs, batch_ys = mnist.train.next_batch(100)
+
       sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 
 if __name__ == '__main__':
-    run()
+    tf.app.run()
