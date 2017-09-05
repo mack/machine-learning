7bc130d7ebe0798fd509037b2a6a2bbf67eb939d
Mack Boudreau <mack.boudreau2@gmail.com>
2017-09-04 22:49:34 -0300

    Added in softmax node

---
+++ NN/comp_graph/operations/softmax.py
@@ -0,0 +1,12 @@
+from operation import Operation
+
+class softmax(Operation):
+    def __init__(self, a):
+        """
+        Args:
+            a: Input node
+        """
+        super().__init__([a])
+
+    def compute(self, a_val):
+        return np.exp(a_val) / np.sum(np.exp(a_val), axis=1)[:, None]
