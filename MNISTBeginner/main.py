826cfd8e9fdaff9db0f9c812ad5cea410fed668b
Mack Boudreau <mack.boudreau2@gmail.com>
2017-09-08 23:39:19 -0300

    Added post request for img

--- MNISTBeginner/main.py
+++ MNISTBeginner/main.py
@@ -1,16 +1,15 @@
 # logistic regression on mnist instead of a superior deep convolution net
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from train import *
 
 app = Flask(__name__)
+c = classifier()
 
-@app.route('/')
+
+@app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
-
-def main():
-    c = classifier()
-    c.predict()
-
-if __name__ == '__main__':
-    main()
+    if (request.method == "POST"):
+        print(request.form['img'])
+        return redirect(request.url)
+    else:
+        return render_template("index.html")
--- MNISTBeginner/static/main.js
+++ MNISTBeginner/static/main.js
@@ -1,4 +1,24 @@
 (function() {
+  // taken from https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
+  function post(path, params, method) {
+    method = method || "post";
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+    document.body.appendChild(form);
+    form.submit();
+  }
+
   function main() {
     // initialize the drawing app
     drawing = new window.app.Drawing();
@@ -9,7 +29,8 @@
 
     drawing.onChange = function() {
       map = drawing.mnistIntensities()
-        
+      map = JSON.stringify(map);
+      post('/', {img: map});
     }
   }
 
