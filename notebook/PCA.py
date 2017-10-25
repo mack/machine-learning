aa10aefa8982864c25b0a6e551d1a36e2b34c185
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-10-24 22:26:19 -0300

    Calculated eigen values

--- notebook/PCA.py
+++ notebook/PCA.py
@@ -1,6 +1,25 @@
 import numpy
+from sklearn.preprocessing import StandardScaler
 
 class PCA(object):
     def __init__(self):
         pass;
-    def compress(self, array):
+    """ Compress
+    Using eigenvalue decomposition to perform principle component analysis.
+    (demonsionality reduction)
+    args:
+        array: n x m array, to Compress
+    return:
+
+    """
+    def compress_with_eigen(self, tensor):
+        # feature scaling (standardize data)
+        tensor_std = StandardScaler().fit_transform(tensor);
+        # covariance matrix
+        tensor_covar = np.cov(tensor.T)
+        # eigen decomposition
+        eig_values, eig_vectors = np.linalg.eig(tensor_covar)
+
+
+    def uncompress(self, tensor):
+        pass;
