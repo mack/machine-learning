dd6b945971307d2add77fe10d2d0413e1f5d075c
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-10-28 09:46:11 -0300

    Plotted data

--- notebook/PCA.py
+++ notebook/PCA.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import plotly.plotly as py
+import plotly
 from plotly.graph_objs import *
-import plotly.tools as tls
 
 class PCA(object):
     def __init__(self, tensor):
@@ -18,7 +18,7 @@ class PCA(object):
     return:
 
     """
-    def compress_with_eigen(self):
+    def compress_with_eigen(self,size):
         # feature scaling (standardize data)
         # covariance matrix
         tensor_covar = np.cov(self.tensor.T)
@@ -33,8 +33,7 @@ class PCA(object):
         eig_pairs.reverse()
 
         # construct projection matrix
-        matrix_w = np.hstack((eig_pairs[0][1].reshape(4,1),
-                      eig_pairs[1][1].reshape(4,1)))
+        matrix_w = np.hstack([eig_pairs[i][1].reshape(4,1) for i in range(size)])
         return self.tensor.dot(matrix_w)
 
 
@@ -43,26 +42,41 @@ class PCA(object):
 
 
 def main():
-    # np.random.seed(0)
-    # x = np.random.rand(4,3) * 10
-    # x[0,0] = 1000
-    # x[0, 1] = 500
-    #
-    #
-    # print(x)
-    # y = np.random.rand(4) * 25
-    # y[0] =100
-    # print(y)
-    # plt.plot(x, y, 'ro')
-    # plt.show()
-
     # load in the iris dataset
     dataframe = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
     dataframe.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
     X = dataframe.iloc[:,0:4].values
     y = dataframe.iloc[:,4].values
-    _X = PCA(X).compress_with_eigen()
+    # _X now has 2 dimensions....
+    _X = PCA(X).compress_with_eigen(2)
+
+    # display data
+    traces = []
+    for name in ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'):
+        trace = Scatter(
+            x=_X[y==name,0],
+            y=_X[y==name,1],
+            mode='markers',
+            name=name,
+            marker=Marker(
+                size=12,
+                line=Line(
+                    color='rgba(217, 217, 217, 0.14)',
+                    width=0.5),
+                opacity=0.8))
+        traces.append(trace)
+
+    data = Data(traces)
+    layout = Layout(xaxis=XAxis(title='PC1', showline=False),
+                    yaxis=YAxis(title='PC2', showline=False))
+    fig = Figure(data=data, layout=layout)
+    plotly.offline.iplot(fig)
+
+
+
+
 
 
 if __name__ == '__main__':
+    plotly.offline.init_notebook_mode(connected=True)
     main()
